a8ab598b93261d8cf122e284bfc2fb386a8c7413
boan1130 <sunmimi28281130@gmail.com>
2025-07-01 10:24:20 +0800

    12132

---
+++ 0701/MatrixMultiplication.java
@@ -0,0 +1,37 @@
+public class MatrixMultiplication {
+
+    public static void main(String[] args) {
+        int[][] A = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] B = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] C = new int[3][3]; 
+
+        
+        for (int i = 0; i < 3; i++) {            
+            for (int j = 0; j < 3; j++) {       
+                C[i][j] = 0;
+                for (int k = 0; k < 3; k++) {    
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        
+        System.out.println("相乘結果為：");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(C[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 0701/searchnumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class searchnumber {
+
+    public static void main(String[] args) {
+        int[] s = {1, 3, 5, 7, 9, 11, 13}; 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入要查找的數字：");
+        int x = scanner.nextInt();
+        boolean found = false;
+
+        // 使用線性搜尋
+        for (int num : s) {
+            System.out.println("比對中: x = " + x + "，目前元素 = " + num);
+            if (num == x) {
+                found = true;
+                break;
+            }
+        }
+
+    
+        if (found) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
